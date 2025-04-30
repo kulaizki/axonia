bf19148679deeb9efa671ff9ec8aae904b6f8623
kulaizki <kulaizke@gmail.com>
2025-05-01 04:05:50 +0800

    feat: add footer component and enhance layout structure for improved UI presentation

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,30 @@
+<footer class="border-t border-gray-700 bg-gray-900/95 py-2 text-white backdrop-blur">
+	<div class="mx-auto flex max-w-6xl flex-row items-center justify-between gap-4 px-6">
+		<p class="my-3 text-center text-xs sm:text-sm md:text-left">
+			&copy; {new Date().getFullYear()}
+			<a
+				href="https://fitzsixto.com/"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="bg-gradient-to-r from-violet-300 to-violet-600 bg-clip-text font-bold text-transparent transition duration-200 ease-in-out [text-shadow:0_0_8px_rgba(0,242,255,0.4)] hover:from-blue-400 hover:to-blue-800"
+			>
+				Fitzsixto
+			</a>. All rights reserved.
+		</p>
+		<div class="flex space-x-4">
+			<a
+				href="https://github.com/kulaizki/planpokr/"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="GitHub"
+				class="flex items-center space-x-2"
+			>
+				<img
+					src="https://skillicons.dev/icons?i=github"
+					alt="GitHub"
+					class="h-10 w-10 transition duration-300 ease-in-out hover:scale-110 hover:opacity-75"
+				/>
+			</a>
+		</div>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-
+	import Footer from '$lib/components/footer.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-col min-h-screen bg-gradient-to-br from-gray-950 via-gray-800 to-violet-400">
+	<main class="flex-grow flex flex-col text-white">
+		{@render children()}
+	</main>
+	<Footer />
+</div>
